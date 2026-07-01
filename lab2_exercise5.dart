7f06f390cf7280b99431c29a27ead51d651eea9b
kasidit-67310379 <kasiditt67@nu.ac.th>
2026-07-01 11:43:59 +0700

    first commit

---
+++ lab2_exercise5.dart
@@ -0,0 +1,48 @@
+import 'dart:developer';
+
+void grade(int score)
+{
+  print("Score : $score");
+  if(score >= 80)
+  {
+    print("Grade A");
+  }
+  else if(score >= 70)
+  {
+    print("Grade B");
+  }
+  else if(score >= 60)
+  {
+    print("Grade C");
+  }
+  else if(score >= 50)
+  {
+    print("Grade D");
+  }
+  else
+  {
+    print("Grade F");
+  }
+  print("---");
+}
+
+void main()
+{
+  grade(85);
+  grade(72);
+  grade(50);
+  grade(40);
+
+  print("Number 1 to 10 :");
+  for(int i =1; i <= 10; i++)
+  {
+    print(i);
+  }
+
+  var fruits = ["Apple", "Banana", "Cherry"];
+  print(" Fruits :");
+  for(var fruit in fruits)
+  {
+    print("- $fruit");
+  }
+}
